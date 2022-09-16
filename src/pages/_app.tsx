97fb092f0cff535eef652717f8e85723927ce7b0
margaiwangara <margaiwangara@gmail.com>
2022-09-16 07:07:27 +0300

    styled components setup

---
+++ src/pages/_app.tsx
@@ -0,0 +1,38 @@
+import { AppProps } from 'next/app';
+import { createGlobalStyle, ThemeProvider } from 'styled-components';
+import theme from '@base/theme';
+import { normalize } from 'polished';
+import Router from 'next/router';
+
+function App({ pageProps, Component }: AppProps) {
+  return (
+    <>
+      <GlobalStyle />
+      <ThemeProvider theme={theme}>
+        <Component {...pageProps} />
+      </ThemeProvider>
+    </>
+  );
+}
+
+const GlobalStyle = createGlobalStyle`
+  ${normalize()}
+  *, *::after, *::before {
+    box-sizing: border-box;
+    margin: 0;
+    padding: 0;
+  }
+  body {
+    font-family: 'Poppins', Arial, Helvetica, sans-serif;
+    font-size: 16px;
+    width: 100%;
+    overflow-x: hidden;
+    font-weight: 400;
+  }
+
+  a {
+    text-decoration: none;
+  }
+`;
+
+export default App;
